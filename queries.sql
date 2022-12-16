5b3178e62b778e5091356ac742856967dc5413f8
john <kiokoj81@gmail.com>
2022-12-16 15:15:38 +0300

    Made changes requested by code reviewer.

--- queries.sql
+++ queries.sql
@@ -13,23 +13,39 @@ BEGIN;
 
 UPDATE animals SET species = 'unspecified';
 
+SELECT species from animals;
+
 ROLLBACK;
+
+SELECT species from animals;
+
+BEGIN;
 
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 
 COMMIT;
 
+SELECT species from animals;
+
+BEGIN;
+
+DELETE FROM animals;
+SELECT *FROM animals;
+ROLLBACK;
+SELECT *FROM animals;
+
 BEGIN;
 DELETE FROM animals WHERE date_of_birth  > '2022-01-02';
 SAVEPOINT delete_2022;
 UPDATE animals SET  weight_kg = ( weight_kg * -1);
 ROLLBACK TO delete_2022;
-UPDATE animals SET  weight_kg = ( weight_kg * -1) WHERE id IN (7,5,8,6);
+UPDATE animals SET  weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 
 SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts < 1;
 SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
-SELECT neutered, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
